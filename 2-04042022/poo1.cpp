5a695f12fc1460f1dcd1a5e9145c04162bad9d87
Henrique Santos <santosrick1705@gmail.com>
2022-04-04 10:41:26 -0300

    Introdução Linguagem C++

---
+++ 2-04042022/poo1.cpp
@@ -0,0 +1,27 @@
+/*Programa para trabalhar com orientação
+a objeto. Usando classes e objetos*/
+
+/*Biblioteca de entrada e
+saída de dados*/
+#include <iostream>
+
+/*Chamada da biblioteca std*/
+using std::cout;
+using std::cin;
+
+class Textos{
+    public:
+    void mensagem1(){
+        cout << "Olá!\n";
+    }
+    void mensagem2(){
+        cout << "Mundo!\n";
+    }
+};
+
+int main(){
+    Textos txt;
+    txt.mensagem1();
+    txt.mensagem2();
+    return 0;
+}
