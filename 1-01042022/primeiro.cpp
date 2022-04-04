6790b783ded2673620b398796ce2d2ab827077bc
Henrique Santos <santosrick1705@gmail.com>
2022-04-04 08:52:36 -0300

    Introdução a Linguagem C++

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,14 @@
+/*Programa para exibir uma mensagem. 
+O código foi escrito em C++*/
+
+/*Importação da biblioteca de 
+entrada e saida de dados*/
+#include <iostream>
+
+int main(){
+    //Saída padrão de dados
+    /*Cout(console out -> Saída na tela de console)*/
+    std::cout << "Bem vindo ao C++\n";
+
+    return 0;
+}
